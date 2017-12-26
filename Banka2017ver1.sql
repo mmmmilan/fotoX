52f4dc24f8354e4d00a454b13cf7210b1bacdd46
Milan Mihajlovic <mmmmilan@gmail.com>
2017-12-26 12:09:09 +0100

    first commit

---
+++ Banka2017ver1.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE Klijent(
+	KlijentId INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
+	KorisnickoIme NVARCHAR(20) NOT NULL,
+	Lozinka NVARCHAR NOT NULL,
+	StanjeNaRacunu DECIMAL (12,3) NOT NULL
+)
+
+GO
+GO
+CREATE PROCEDURE NadjiKlijenta
+(
+	@KorisnickoIme VARCHAR(20),
+	@Lozinka VARCHAR(20),
+	@KlijentId INT OUTPUT
+)
+AS
+	SELECT @KlijentId = KlijentId
+	FROM Klijent
+	WHERE KorisnickoIme = @KorisnickoIme
+	AND Lozinka = @Lozinka
+
+	IF @@ROWCOUNT < 1
+		SELECT @KlijentId=0
+GO
+
---
+++ SQLQuery3.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE Klijent(
+KlijentId int IDENTITY (1,1) NOT NULL PRIMARY KEY,
+KorisnickoIme NVARCHAR (20) NOT NULL,
+Lozinka NVARCHAR (20) NOT NULL,
+StanjeNaRacunu DECIMAL (12,3)
+)
+go
+go
+CREATE PROCEDURE NadjiKlijenta
+(
+	@KorisnickoIme VARCHAR(20),
+	@Lozinka VARCHAR(20),
+	@KlijentId INT OUTPUT
+)
+AS
+	SELECT @KlijentId = KlijentId
+	FROM Klijent
+	WHERE KorisnickoIme = @KorisnickoIme
+	AND Lozinka = @Lozinka
+
+	IF @@ROWCOUNT < 1
+		SELECT @KlijentId=0
